421620134d95889eda26808f58bc0c3798e0ba18
Jérôme AUGUSTE <jerome_auguste@outlook.fr>
2023-01-20 12:08:05 +0100

    Smooth scrolling and smooth opacity using ChatGPT

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
+// Function to check if an element is in the viewport
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+};
+
 // Smooth scrolling when clicking on navigation links
 const navLinks = document.querySelectorAll('.nav-link');
 navLinks.forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
-    document.querySelector(link.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+    navLinks.forEach(link => link.classList.remove('active'));
+    link.classList.add('active');
+    const section = document.querySelector(link.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
   });
 });
+
 
 // Fade-in effect for sections
 const sections = document.querySelectorAll('section');
-sections.forEach(section => {
-  section.style.opacity = 0;
-});
-
-window.addEventListener('scroll', () => {
+function handleScroll() {
   sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const sectionBottom = section.getBoundingClientRect().bottom;
     if (isInViewport(section)) {
       section.style.opacity = 1;
+    } else if (section.style.opacity === "1") {
+      // don't change the opacity if it's already 1
+    } else {
+      section.style.opacity = 0.2;
     }
   });
-});
 
-// Function to check if an element is in the viewport
-function isInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
 }
+
+document.addEventListener('scroll', handleScroll);
